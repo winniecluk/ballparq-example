cc89105f0b475c9d81a8b9a2d11599be5589ac2e
Winnie Luk <winniecluk@gmail.com>
2017-01-12 19:04:13 -0800

    gulp rename package

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+
 
 gulp.task('styles', function(){
   return gulp.src('public/css/*.css')
@@ -14,10 +16,8 @@ gulp.task('styles', function(){
 gulp.task('scripts', function(){
   return gulp.src(['public/js/app/app.module.js', 'public/js/app/app.routes.js', 'public/js/app/**/*.js'])
     .pipe(concat('all.js'))
-    // .pipe(uglify())
-    .pipe(babel({
-      presets: ['es2015']
-    }))
+    .pipe(rename('uglify.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('public'));
 })
 
